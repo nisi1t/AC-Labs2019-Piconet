2552ba9b11a049e40afa577cf61b3eeef9203ea1
Mihai Nisipeanu <mihai.nisipeanu@piconet.ro>
2019-05-08 13:13:44 +0300

    strings update

--- app/src/main/java/ro/nisi/android/ac_labs2019/LoginActivity.java
+++ app/src/main/java/ro/nisi/android/ac_labs2019/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -62,7 +63,7 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        showProgressDialog("Create new account", "Creating ...");
+        showProgressDialog(getString(R.string.create_account_txt), "Creating ...");
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
